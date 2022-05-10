1276dfcec63eaf179e0fc76b73456b0ed811477d
limyel <limyel@outlook.com>
2022-05-10 15:09:50 +0800

    clear

--- src/request/api/about.js
+++
@@ -1,8 +0,0 @@
-import {get} from "@/request/http";
-
-let baseURL = '/blog/posts/about'
-
-export function apiAbout() {
-    let url = baseURL;
-    return get(url);
-}
--- src/request/api/login.js
+++
@@ -1,6 +0,0 @@
-import {get} from "@/request/http";
-
-export function apiUserInfo(code) {
-    let url = '/blog/oauth/github';
-    return get(url, {'code': code});
-}
