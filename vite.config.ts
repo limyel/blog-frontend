a46decb9b407f000475f9a555120e2d7a869c431
limyel <limyel@outlook.com>
2022-05-13 16:56:57 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from "path";
+const pathResolve = (dir: string) => resolve(__dirname, dir);
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins:[
+    vue()
+  ],
+  resolve: {
+    alias: {
+      "@": pathResolve("./src"), // 别名
+    }
+  }
 })
