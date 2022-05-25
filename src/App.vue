ab94d9ae51ca002105603e151853564a7a0e7038
limyel <limyel@outlook.com>
2022-05-25 14:37:35 +0800

    vue3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import Theme from './pages/Theme.vue'
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <router-view/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
-  @import "./assets/css/reset.css";
-  @font-face {
-    font-family: 'oppos-sons';
-    src: url('./assets/font/OPPOSans-R.ttf')
-  }
-
-  body * {
-    font-family: 'oppos-sons',fangsong;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
 </style>
