4c83928d345ed836f72a477b6e07ba0e2b5a1e4c
limyel <limyel@outlook.com>
2022-05-26 23:23:40 +0800

    tag detail

---
+++ src/pages/tag_detail.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <div class="tag-title">
+      Python
+    </div>
+
+    <div class="post-list">
+      <div class="archive-block">
+        <div class="archive-year">
+          2022
+        </div>
+        <div class="archive-item">
+          <div class="archive-item-date">
+            五月 26
+          </div>
+          <div class="archive-item-title">
+            <a href="">Python 从入门到入坟</a>
+          </div>
+        </div>
+
+        <div class="archive-item">
+          <div class="archive-item-date">
+            五月 26
+          </div>
+          <div class="archive-item-title">
+            <a href="">Python 从入门到入坟</a>
+          </div>
+        </div>
+
+        <div class="archive-item">
+          <div class="archive-item-date">
+            五月 26
+          </div>
+          <div class="archive-item-title">
+            <a href="">Python 从入门到入坟</a>
+          </div>
+        </div>
+
+        <div class="archive-item">
+          <div class="archive-item-date">
+            五月 26
+          </div>
+          <div class="archive-item-title">
+            <a href="">Python 从入门到入坟</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="archive-block">
+      <div class="archive-year">
+        2022
+      </div>
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="archive-block">
+      <div class="archive-year">
+        2022
+      </div>
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+
+      <div class="archive-item">
+        <div class="archive-item-date">
+          五月 26
+        </div>
+        <div class="archive-item-title">
+          <a href="">Python 从入门到入坟</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="page">
+      <button class="pre-page">上一页</button>
+      <button class="next-page">下一页</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tag-title {
+  font-size: 2rem;
+  margin-top: 35px;
+}
+
+.archive-year {
+  font-size: 1.5rem;
+  margin: 35px 0;
+}
+
+.archive-item {
+  display: flex;
+  flex-direction: row;
+  line-height: 1.6rem;
+  width: 100%;
+  margin: 10px 0;
+}
+
+.archive-item-date {
+  margin-right: 50px;
+}
+
+.archive-item-title {
+  flex-grow: 1;
+}
+
+.page {
+  margin-top: 35px;
+}
+.next-page, .pre-page {
+  background-color: #ffffff;
+}
+.next-page {
+  margin-left: 10px;
+}
+.pre-page {
+  margin-right: 10px;
+}
+</style>
