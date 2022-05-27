e7640665feeb8a3cd372ee7001ea6c52a292c186
limyel <limyel@outlook.com>
2022-05-27 10:35:50 +0800

    清理注释

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,12 +11,6 @@
       <router-link :to="{name: 'Tag'}" class="navication-item" :class="activeNav === 'Tag'? 'nav-item-active': ''">标签</router-link>
       <router-link :to="{name: 'Search'}" class="navication-item" :class="activeNav === 'Search'? 'nav-item-active': ''">搜索</router-link>
       <router-link :to="{name: 'About'}" class="navication-item" :class="activeNav === 'About'? 'nav-item-active': ''">关于</router-link>
-<!--      <router-link :to="{name: 'Archives'}" class="container font-3 justify-content-center mr-8" :class="activeNav === 'Archives'? 'nav-item-active': ''">归档</router-link>-->
-<!--      <router-link :to="{name: 'Tags'}" class="container font-3 justify-content-center mr-8" :class="activeNav === 'Tags'? 'nav-item-active': ''">标签</router-link>-->
-<!--      <router-link :to="{name: 'Weekly'}" class="container font-3 justify-content-center mr-8" :class="activeNav === 'Weekly'? 'nav-item-active': ''">周报</router-link>-->
-<!--      <router-link :to="{name: 'Search'}" class="container font-3 justify-content-center mr-8" :class="activeNav === 'Search'? 'nav-item-active': ''">搜索</router-link>-->
-<!--      <router-link :to="{name: 'About'}" class="container font-3 justify-content-center mr-8" :class="activeNav === 'About'? 'nav-item-active': ''">关于</router-link>-->
-<!--      <router-link to="" class="container font-3 justify-content-center mr-8">吐槽</router-link>-->
     </div>
   </div>
 </template>
